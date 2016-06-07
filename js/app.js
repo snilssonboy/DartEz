3d1e26c032b269f21423d6363e352f65938011bf
snilssonboy <simon.nilsson@dohi.se>
2016-06-07 11:14:32 +0200

    Testing changes on prod

--- js/app.js
+++ js/app.js
@@ -19,50 +19,170 @@ var game;
 
 $(document).ready(function(){
 
-	$("#dartboard #areas g").children().hover(
-		function(){
-			$(this).css("opacity","0.6");
-		},
-		function(){
-			$(this).css("opacity","1");
-		}
-	)
-
 	$("#dartboard #areas g").children().mousedown(function(){
 
 		if(hits.length < 3 && typeof game != 'undefined'){
 			hits.push($(this).context.id);
-			$(this).css("fill", "#BDE7F5");
+			//$(this).css("fill", "#BDE7F5");
 			$("#hit-items").append('<li class="list-group-item list-hit" id="hits-item">' + $(this).context.id + '<button type"button" onclick="removeHit(\'' + $(this).context.id + '\')" class="btn-remove-hit pull-right">X</button></li>');
 			$("#total-score").html(calculateScore().toString());
 		}else if(typeof game == 'undefined'){
-			window.alert("Tryck på den blåa användarknappen för att lägga till spelare, eller den gröna play-knappen för att starta ett nytt spel!");
+			//window.alert("Tryck på den blåa användarknappen för att lägga till spelare, eller den gröna play-knappen för att starta ett nytt spel!");
 			$("#managePlayers").addClass("btn-primary");
 			$("#openStart").addClass("btn-success");
 		}else{
-			window.alert("Du har redan 3 markerade träffar, ta bort från panelen till vänster för att göra ändringar.");
+			//window.alert("Du har redan 3 markerade träffar, ta bort från panelen till vänster för att göra ändringar.");  
 		}
-		
-	});
 
-	$("#dartboard #areas g").children().mouseup(function(){
-			$(this).css("fill", "");
-	});
 
-	players.push(new Player("Simon", false, 0, []));
-	players.push(new Player("Jari", false, 0, []));
-	players.push(new Player("Kim", false, 0, []));
-	players.push(new Player("Erik", false, 0, []));
-	players.push(new Player("Jonas", false, 0, []));
-	players.push(new Player("Karl", false, 0, []));
-	players.push(new Player("Peter", false, 0, []));
-	players.push(new Player("Erik", false, 0, []));
-	players.push(new Player("Hugo", false, 0, []));
+		showHits();
+	});
 
 });
 
-function showHits(){
+players.push(new Player("Simon", false, 0, []));
+players.push(new Player("Jari", false, 0, []));
+players.push(new Player("Kim", false, 0, []));
+players.push(new Player("Erik", false, 0, []));
+players.push(new Player("Jonas", false, 0, []));
+players.push(new Player("Karl", false, 0, []));
+players.push(new Player("Peter", false, 0, []));
+players.push(new Player("Erik", false, 0, []));
+players.push(new Player("Hugo", false, 0, []));
 
+var outs = {
+	"170": "T20, T20, DB",	  			   	
+	"167": "T20, T19, DB",	   				  	
+	"164": "T20, T18, DB",    				       		
+	"161": "T20, T17, DB",	       	
+	"160": "T20, T20, D20",		  				  	
+	"158": "T20, T20, D19",	   		
+	"157": "T20, T19, D20",	   		
+	"156": "T20, T20, D18",	   		
+	"155": "T20, T15, DB",   		
+	"154": "T20, T18, D20",	   		
+	"153": "T20, T19, D18",	   		
+	"152": "T20, T20, D16",	   		
+	"151": "T20, T17, D20",	   		
+	"150": "T20, T18, D18",	  		
+	"149": "T20, T19, D16",	  		
+	"148": "T20, T16, D20",	  		
+	"147": "T20, T17, D18",	   		
+	"146": "T20, T18, D16",	   		
+	"145": "T20, T15, D20",	   		
+	"144": "T20, T20, D12",	   		
+	"143": "T20, T17, D16",	   		
+	"142": "T20, T14, D20",	   		
+	"141": "T20, T15, D18",	   		
+	"140": "T20, T16, D16",	   		
+	"139": "T20, T13, D20",			
+	"138": "T20, T14, D18",	
+	"137": "T17, T18, D16",
+	"136": "T20, T20, D8",	
+	"135": "T20, T15, D15",	
+	"134": "T20, T14, D16",	
+	"133": "T20, T19, D8",	
+	"132": "T20, T20, D6",	
+	"131": "T20, T13, D16",	
+	"130": "T20, T18, D8",	
+	"129": "T19, T20, D6",	
+	"128": "T18, T14, D16",	
+	"127": "T19, T18, D8",	
+	"126": "T19, T19, D6",	
+	"125": "B, T20, D20",	
+	"124": "T20, D16, D16",	
+	"123": "T19, T16, D9",	
+	"122": "T18, T20, D4",	
+	"121": "T20, T15, D8",	
+	"120": "T20, 20, D20",	
+	"119": "T19, T10, D16",	
+	"118": "T20, 18, D20",	
+	"117": "T20, 17, D20",	
+	"116": "T20, 16, D20",	
+	"115": "T20, 15, D20",	
+	"114": "T20, 14, D20",	
+	"113": "T20, 13, D20",	
+	"112": "T20, 20, D16",	
+	"111": "T20, 19, D16",	
+	"110": "T20, 18, D16",	
+	"109": "T20, 17, D16",	
+	"108": "T20, 16, D16",	
+	"107": "T19, 18, D16",	
+	"106": "T20, 14, D16",	
+	"105": "T20, 13, D16",
+	"104": "T18, 18, D16",
+	"103": "T20, 11, D16",
+	"102": "T20, 10, D16",
+	"101": "T17 18, D16",
+	"100": "T20, D20",	
+	"99": "T19, 10, D16",
+	"98": "T20, D19",		
+	"97": "T19, D20",		
+	"96": "T20, D18",		
+	"95": "T15, 18, D16",
+	"94": "T18, D20",		
+	"93": "T19, D18",		
+	"92": "T20, D16",		
+	"91": "T17, D20",		
+	"90": "T18, D18",		
+	"89": "T19, D16",		
+	"88": "T16, D20",		
+	"87": "T17, D18",		
+	"86": "T18, D16",		
+	"85": "T15, D20",		
+	"84": "T20, D12",		
+	"83": "T17, D16",		
+	"82": "T14, D20",		
+	"81": "T15, D18",		
+	"80": "T16, D16",		
+	"79": "T13, D20",		
+	"78": "T14, D18",		
+	"77": "T15, D16",		
+	"76": "T20, D8",		
+	"75": "T15, D15",		
+	"74": "T14, D16",		
+	"73": "T19, D8",		
+	"72": "T20, D6",
+	"71": "T13, D16",
+	"70": "T18, D8",
+	"69": "T19, D6",
+	"68": "T16, D10",
+	"67": "T17, D8",
+	"66": "T10, D18",
+	"65": "T15, D10",
+	"64": "D16, D16",
+	"63": "T13, D12",
+	"62": "T10, D16",
+	"61": "T15, D8",
+	"60": "20, D20",
+	"59": "19, D20",
+	"58": "18, D20",
+	"57": "17, D20",
+	"56": "16, D20",
+	"55": "15, D20",
+	"54": "14, D20",
+	"53": "13, D20",
+	"52": "20, D16",
+	"51": "19, D16",
+	"50": "18, D16",
+	"49": "17, D16",
+	"48": "16, D16",
+	"47": "15, D16",
+	"46": "14, D16",
+	"45": "13, D16",
+	"44": "12, D16",
+	"43": "11, D16",
+	"42": "10, D16",
+	"41": "9, D16",
+	"40": "D20"	
+};
+
+function showHits(){
+	if(hits.length < 1){
+		$("#hits-container").addClass("hidden");
+	}else{
+		$("#hits-container").removeClass("hidden");
+	}
 }
 
 function abortCurrentGame(){
@@ -80,10 +200,14 @@ function abortCurrentGame(){
 
 		updatePlayerList();
 		hits = [];
+		/*
 		$("#hit-items").html("");
 		$("#abort-button").toggleClass("hidden");
 		$("#total-score").html("");
 		$("#ongoing-game-table").html("");
+		$("#button-panel").toggleClass("hidden");
+		*/
+		location.reload();
 	}
 }
 
@@ -183,10 +307,7 @@ function startNewGame(){
 		gamemode = 301;
 	}else if($("#mode_501").prop("checked")){
 		gamemode = 501;
-	}else{
-		return window.alert("Du har inte valt något game mode, gör om, gör rätt!");
 	}
-
 
 	var activePlayers = [];
 
@@ -200,6 +321,8 @@ function startNewGame(){
 		return window.alert("Inte nog med spelare");
 	}else{
 		shuffle(activePlayers);
+
+		//Create game with variables: Players[], round(int), turn(int), victor(string)
 		game = new Game(activePlayers, 1, 1, "");
 		for(var i = 0; i < game.players.length; i++){
 			game.players[i].score = gamemode;
@@ -207,6 +330,7 @@ function startNewGame(){
 		console.log("Spel startat, game mode: " + gamemode);
 		gameManager(game);
 		$("#abort-button").toggleClass("hidden");
+		$("#button-panel").toggleClass("hidden");
 	}
 	
 }
@@ -241,7 +365,8 @@ function addScore(){
 	}
 
 	gameManager(game);
-	$("#total-score").html(calculateScore().toString());
+
+	showHits();
 }
 
 function redoLastTurn(){
@@ -263,8 +388,6 @@ function redoLastTurn(){
 
 	game.players[game.turn - 1].hits[game.players[game.turn - 1].hits.length - 1] = [];
 
-	//console.log(hits);
-
 	$("#hit-items").html("");
 
 	for(var i = 0; i < hits.length; i++){
@@ -276,18 +399,46 @@ function redoLastTurn(){
 	}else{
 		game.players[game.turn - 1].score += calculateScore();
 	}
-	
-
-	$("#total-score").html(calculateScore().toString());
 
 	gameManager(game);
+
+	showHits();
+}
+
+function getOut(score){
+
+	console.log("söker efter out från " + score);
+
+	var out = outs[score];
+
+	console.log(out + " ?");
+
+	if(typeof out != 'undefined'){
+		return out;
+	}else{
+		return "";
+	}
 }
 
 function gameManager(gameobj){
 	$("#ongoing-game-table").html("");
 
 	for(var i = 0; i < gameobj.players.length; i++){
-		$("#ongoing-game-table").append("<tr><th>" + (i + 1) + "</th><td>" + gameobj.players[i].name + "</td><td>" + gameobj.players[i].score + "</td><td>t20,t20,t20</td></tr>");
+
+		var lastHits = [];
+		var lastHitsString = "";
+
+		if(typeof gameobj.players[i].hits[gameobj.players[i].hits.length - 1] != 'undefined'){
+			lastHits.push(gameobj.players[i].hits[gameobj.players[i].hits.length - 1]);
+
+			for(var x = 0; x < lastHits.length; x++){
+				lastHitsString += lastHits[x] + ", ";
+			}
+
+			lastHitsString = lastHitsString.substring(0, lastHitsString.length - 2);
+		}
+
+		$("#ongoing-game-table").append("<tr><th>" + (i + 1) + "</th><td>" + gameobj.players[i].name + "</td><td>" + gameobj.players[i].score + "</td><td>" + getOut(gameobj.players[i].score) + "</td><td>" + lastHitsString +  "</td></tr>");
 	}
 
 	$("#ongoing-game-table tr:nth-child(" + gameobj.turn + ")").toggleClass("active")
